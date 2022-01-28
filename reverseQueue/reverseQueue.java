066c1877054bf06f271a7d269b39643c1d9110a6
Laxman Poudel <coderlaxman@gmail.com>
2022-01-28 07:54:07 -0500

    reverseQueue Problem

---
+++ reverseQueue/reverseQueue.java
@@ -0,0 +1,31 @@
+package com.laxman;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        reverse(queue);
+        System.out.println(queue);
+    }
+
+    public  static  void reverse( Queue <Integer> queue)
+    {
+        Stack <Integer> st = new Stack<>();
+        while(!queue.isEmpty())
+        {
+            st.push(queue.remove());
+        }
+
+        while (!st.isEmpty())
+        {
+            queue.add((st.pop()));
+        }
+    }
+}
