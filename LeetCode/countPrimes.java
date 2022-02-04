d7cda58376f20e9c988660072ab1426050fc30e1
Laxman Poudel <coderlaxman@gmail.com>
2022-02-03 22:15:36 -0500

    countPrimes

---
+++ LeetCode/countPrimes.java
@@ -0,0 +1,60 @@
+
+class Solution {
+    public int countPrimes(int n) {
+        
+        boolean[] prime = new boolean[n];
+        for(int i=0; i<prime.length; i++)
+        {
+            prime[i] = true;
+        }
+        
+        for(int i=2; i*i< prime.length; i++)
+        {
+        
+        if(prime[i])
+            
+            for(int j=i; i*j<prime.length; j++)
+            {
+                prime[i*j] = false;
+            }
+        }
+        
+        int count = 0;
+        for(int i=2; i<prime.length; i++)
+        {
+            if(prime[i])
+            count++;
+        }
+         return count;
+    }
+}
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
