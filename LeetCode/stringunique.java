05043e09f690ccc88c39e2d4ceb4cad7c24c4695
Laxman Poudel <coderlaxman@gmail.com>
2022-02-22 22:57:49 -0500

    Update stringunique.java

--- LeetCode/stringunique.java
+++ LeetCode/stringunique.java
@@ -13,7 +13,14 @@ class Solution {
                 }
             }
             
-        
+        for( int i=0; i<s.length(); i++)
+        {
+             char ch = s.charAt(i);
+        if(map.get(ch) ==1)
+        {
+            return i;
+        }
+        }
             return -1;
     }
 }
